2ab86431cab7868383ccb5f69a146e7f73255847
Khalid Alharrasi <oboy970@outlook.com>
2026-07-30 22:37:15 +0400

    Fix employee service model path

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../environments/environment';
 import { Observable } from 'rxjs';
-import { Employee } from '../../models/employee';
+import { Employee } from '../models/employee';
 
 
 @Injectable({
